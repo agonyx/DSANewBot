60c81580e137a6c02b27d1c813a871ebcafea8f2
agonyx <j-l.winkler@vanity.pw>
2026-08-14 15:58:48 +0200

    allow startup without optional event modules

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ for (const file of commandFiles) {
 
 // Event handling
 const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+const eventFiles = fs.existsSync(eventsPath) ? fs.readdirSync(eventsPath).filter(file => file.endsWith('.js')) : [];
 
 for (const file of eventFiles) {
     const filePath = path.join(eventsPath, file);
